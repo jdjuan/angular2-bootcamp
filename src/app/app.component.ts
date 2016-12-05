c85a09fe5d8d5f141af652763697694bd07eb81b
Juan Herrera <david.juanherrera@gmail.com>
2016-12-05 14:17:20 -0500

    Add ngModel to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Person } from './person';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  person = new Person(1, "Juan", "Batman");
 }
