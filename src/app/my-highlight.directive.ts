e0816e79ed2d2f711013b6b23980be81f86e98fe
Juan Herrera <david.juanherrera@gmail.com>
2016-12-05 16:46:51 -0500

    Add host listeners

--- src/app/my-highlight.directive.ts
+++ src/app/my-highlight.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appMyHighlight]'
 })
 export class MyHighlightDirective {
 
-  constructor(el: ElementRef) {
-       el.nativeElement.style.backgroundColor = 'yellow';
-    }
-    
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor = 'yellow';
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = null;
+  }
+
 }
