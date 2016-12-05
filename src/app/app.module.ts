be7ec678c2a0b4d5066201712e27d33b9f9980f2
Juan Herrera <david.juanherrera@gmail.com>
2016-12-05 16:41:53 -0500

    Add highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyHighlightDirective } from './my-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHighlightDirective
   ],
   imports: [
     BrowserModule,
