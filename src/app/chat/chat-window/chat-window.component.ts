5045921424f4014917038b67e76e3d4a41629a3d
Felipe Jaramillo Gómez <pipeluck@gmail.com>
2016-12-09 13:03:16 -0500

    Add input class

--- src/app/chat/chat-window/chat-window.component.ts
+++ src/app/chat/chat-window/chat-window.component.ts
@@ -2,29 +2,32 @@ import { UserService } from './../../core/user.service';
 import { Message } from './../shared/message.model';
 import { ChatRoomService } from './../shared/chatroom.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable,Subscription } from 'rxjs/Rx';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.css']
 })
-export class ChatWindowComponent implements OnInit,OnDestroy {
+export class ChatWindowComponent implements OnInit, OnDestroy {
   private timer: Observable<number>;
   private messages: Array<Message>;
-  private subscription : Subscription;
+  private subscription: Subscription;
 
-  constructor(private chatService: ChatRoomService, private userService:UserService) { 
+  constructor(private chatService: ChatRoomService, private userService: UserService) {
     this.messages = new Array<Message>();
   }
 
   ngOnInit() {
     this.timer = Observable.timer(0, 1000);
-    this.subscription =  this.timer.map(t => this.chatService.getMessages()).subscribe(messages => this.messages = messages);
+    this.subscription = this.timer.map(t => this.chatService.getMessages()).subscribe(messages => this.messages = messages);
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
+  isUser(author: string) {
+    return this.userService.UserName == author;
+  }
 }
