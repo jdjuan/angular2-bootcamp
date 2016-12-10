7a46c1ed88abbc844c56e18823125b1ce414fa81
Felipe Jaramillo Gómez <pipeluck@gmail.com>
2016-12-09 21:36:51 -0500

    Solution to the example

--- src/app/chat/chat-window/chat-window.component.ts
+++ src/app/chat/chat-window/chat-window.component.ts
@@ -5,29 +5,33 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
-  selector: 'app-chat-window',
-  templateUrl: './chat-window.component.html',
-  styleUrls: ['./chat-window.component.css']
+    selector: 'app-chat-window',
+    templateUrl: './chat-window.component.html',
+    styleUrls: ['./chat-window.component.css']
 })
 export class ChatWindowComponent implements OnInit, OnDestroy {
-  private timer: Observable<number>;
-  private messages: Array<Message>;
-  private subscription: Subscription;
+    private timer: Observable<number>;
+    private messages: Array<Message>;
+    private subscription: Subscription;
 
-  constructor(private chatService: ChatRoomService, private userService: UserService) {
-    this.messages = new Array<Message>();
-  }
+    constructor(private chatService: ChatRoomService, private userService: UserService) {
+        this.messages = new Array<Message>();
+    }
 
-  ngOnInit() {
-    this.timer = Observable.timer(0, 1000);
-    this.subscription = this.timer.map(t => this.chatService.getMessages()).subscribe(messages => this.messages = messages);
-  }
+    ngOnInit() {
+        this.timer = Observable.timer(0, 1000);
+        this.subscription = this.timer.subscribe(t => {
+            this.chatService.getMessages().then(
+                messages => this.messages = messages
+            )
+        });
+    }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
-  isUser(author: string) {
-    return this.userService.UserName == author;
-  }
+    isUser(author: string) {
+        return this.userService.UserName == author;
+    }
 }
--- src/app/chat/shared/chatroom.service.ts
+++ src/app/chat/shared/chatroom.service.ts
@@ -1,19 +1,30 @@
 import { Message } from './message.model';
 import { Injectable } from '@angular/core';
+import { Http,Headers } from '@angular/http'
 
 @Injectable()
 export class ChatRoomService {
+    private APIEndPoint = 'http://localhost:3000/'
+    private headers = new Headers({'Content-Type': 'application/json'});
 
-    private messages:Array<Message>;
-    constructor() { 
-        this.messages  = new Array<Message>();
+    private messages: Array<Message>;
+    constructor(private http: Http) {
+        this.messages = new Array<Message>();
     }
 
-    postMessage(message:Message){
-        this.messages.push(message)
+    postMessage(message: Message) {
+     return this.http.post(this.APIEndPoint,message, {headers: this.headers}).toPromise();
     }
 
-    getMessages(){
-        return this.messages;
+    getMessages():Promise<Array<Message>> {
+        return this.http.get(this.APIEndPoint)
+                    .toPromise()
+                    .then(response => response.json() as Message[])
+                    .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
