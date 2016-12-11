55f1637e5c4182c1ada695095f48b1f5740f72d2
Felipe Jaramillo Gómez <pipeluck@gmail.com>
2016-12-11 09:53:26 -0500

    Operators example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Component, ViewChild } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  searchFormControl:FormControl = new FormControl()
+  friendsSource = ['Lucía','Fernando','German','Camilo','Alejandra', 'Juan', 'Felipe'];
+  friends:Observable<string[]>;
+
+  constructor(){
+   this.friends = this.searchFormControl.valueChanges
+                                .debounceTime(500)
+                                .distinctUntilChanged()
+                                .switchMap(value => this.getFriends(value))
+                                .startWith(this.friendsSource);
+  }
+  
+  getFriends(name:string){
+    var filtered = name?  this.friendsSource.filter(friendName => friendName.toLowerCase().includes(name)): this.friendsSource;
+    return  Observable.of(filtered)
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CoreModule } from './core/core.module';
 import { ChatModule } from './chat/chat.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     CoreModule,
     ChatModule
