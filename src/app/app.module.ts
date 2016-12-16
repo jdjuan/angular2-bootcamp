c4a5fedde02a5c75bfa173e38885de8d993aef4c
Juan Herrera <david.juanherrera@gmail.com>
2016-12-16 15:28:04 -0500

    Add root routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { AboutModule } from './about/about.module';
+import { NewsModule } from './news/news.module';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +19,21 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeModule,
+    NewsModule,
+    AboutModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'news', component: NewsComponent
+      },
+      {
+        path: 'about', component: AboutComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
